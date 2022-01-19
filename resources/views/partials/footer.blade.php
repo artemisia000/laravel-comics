22812facbabb6140b843cdc746337accadd7b555
artemisia000 <misiapierobon@gmail.com>
2022-01-19 14:55:58 +0100

    start

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,33 @@
+
+<footer>
+    <div class="container">
+
+        <div>
+            <a href="">
+                SIGN-UP NOW!
+            </a>
+        </div>
+
+        <div>
+            <div>
+                FOLLOW US
+            </div>
+            <div>
+                <a href=""></a>
+            </div>
+            <div>
+                <a href=""></a>
+            </div>
+            <div>
+                <a href=""></a>
+            </div>
+            <div>
+                <a href=""></a>
+            </div>
+            <div>
+                <a href=""></a>
+            </div>
+
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,24 @@
+
+<header>
+    <div class="container">
+        <a href="">
+            logo
+        </a>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="">
+                        COMICS
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        NEWS
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+    </div>
+</header>
