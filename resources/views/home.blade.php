753c7b5e46a927cd0ee7216317dd45eb0ab131b3
artemisia000 <misiapierobon@gmail.com>
2022-01-19 20:26:17 +0100

    stampa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,11 @@
     <main class="container" >
 
         <div class="cards">
-            @foreach($comics as $comic)
+            @foreach ($comics as $comic)
               <div class="card">
-                  <img src=" {{ $comic['thumb'] }} " alt="">
+                  <img src=" {{ $comic['thumb'] }} " alt="{{ $comic['series'] }}">
               </div>
-              <h3> {{ $comic['series'] }} </h3>
+              <h3>{{ $comic['title'] }}</h3>
             @endforeach
         </div>
         
